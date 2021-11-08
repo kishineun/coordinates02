5f32582c1be889ca645b0e08517fd4819184f6e8
kishineun <89367817+kishineun@users.noreply.github.com>
2021-11-08 11:45:24 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -13,24 +13,35 @@ addEventListener('load', function () {
     let mouseX=0;
     let mouseY=0;
     
-    document.addEventListener('mousemove', (event) => {
+    document.addEventListener('mousemove',function(event){
         mouseX = event.clientX;
         mouseY = event.clientY;
         //console.log(mouseX + "px" + mouseY + "px");
         //console.log(`${mouseX} px ${mouseY} px백틱키사용`);
+        gsap.to(".horizontal",.5,{y:mouseY});
+        gsap.to(".vertical",.5,{x:mouseX});
+        gsap.to(".target",.5, {x:mouseX, y:mouseY});
+        gsap.to(".tag",.5,{x:mouseX, y:mouseY});
     });
 
-    function animate(){
-        requestAnimationFrame(animate);
-        curX += (mouseX - curX) * 0.1;
-        curY += (mouseY - curY) * 0.1;
+// function animate(){
+//     requestAnimationFrame(animate);
+//         curX += (mouseX - curX) * 0.1;
+//         curY += (mouseY - curY) * 0.1;
     
     
-        horizontal.style.transform = `translateY(${curY}px)`;
-        vertical.style.transform = `translateX(${curX}px)`;
-        target.style.transform = `translate(${curX - targetHalfWidth}px, ${curY - targetHalfHeight}px)`;
-        tag.style.transform = `translate(${curX}px, ${curY}px)`;
-        tag.innerHTML = `${Math.ceil(curX)}px ${Math.ceil(curY)}px`;
-    }
-    animate();
+//         horizontal.style.transform = `translateY(${curY}px)`;
+//         vertical.style.transform = `translateX(${curX}px)`;
+//         target.style.transform = `translate(${curX - targetHalfWidth}px, ${curY - targetHalfHeight}px)`;
+//         tag.style.transform = `translate(${curX}px, ${curY}px)`;
+//         tag.innerHTML = `${Math.ceil(curX)}px ${Math.ceil(curY)}px`;
+//     }
+//     animate();
+        const aniDuration = 2;
+
+        gsap.to("h2.title",{duration:1, opacity:0.3});
+        gsap.to(".box",{duration:2,x:300,ease:"elastic.out(1, 0.3)",  repeat:-1, yoyo:true});
+        gsap.to(".green",{duration:3, rotation:360, scale:0.5, yoyo:true,  repeat:-1});
+        
 });
+
